297024d27bcfa757b936279c5233c2cec2e214ae
shoveria <shoveria@users.noreply.github.com>
2020-06-21 14:36:57 -0400

    Changed content.

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -8,7 +8,7 @@ exports.handler = async function Index () {
      * Basic bio
      */
     fullname: 'Your Name', // ←  Start by adding your name!
-    title: 'My personal site!',
+    title: '',
     occupation: 'Artist & Photographer',
     location: 'West Glacier, MT',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
